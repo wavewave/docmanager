618f3689d7dcda913547de9e4ecaa46d5d425f38
ianwookim <ianwookim@gmail.com>
2011-11-17 15:00:45 +0000

    add all documents

--- lib/Application/DocManager/Data/Document.hs
+++ lib/Application/DocManager/Data/Document.hs
@@ -7,6 +7,25 @@ doclist = [ Document "C"  "002" "" TeX
           , Document "DI" "001" "" TeX
           , Document "HR" "009" "" TeX
           , Document "HR" "010" "" TeX
+          , Document "IX" "001" "" TeX
+          , Document "L"  "001" "" TeX
+          , Document "L"  "002" "" TeX
+          , Document "L"  "003" "" TeX
+          , Document "L"  "004" "" TeX
+          , Document "PP" "001" "" TeX
+          , Document "T"  "010" "" TeX
+          , Document "TD" "001" "" TeX 
+          , Document "TD" "002" "" TeX
+          , Document "TH" "001" "" TeX
+          , Document "TI" "001" "" TeX 
+          , Document "TI" "002" "" TeX 
+          , Document "TJ" "026" "" TeX
+          , Document "TJ" "027" "" TeX 
+          , Document "TP" "001" "" TeX 
+          , Document "TP" "002" "" TeX
+          , Document "TP" "003" "" TeX 
+          , Document "TP" "004" "" TeX 
+          , Document "V"  "001" "" TeX
           , Document "TP" "005" "" Xoj
           , Document "TP" "006" "" Xoj
           ] 
